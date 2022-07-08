fb2c9eb251fc31d8d4db32a6a6996cc7834e80ca
ahmos <kamaboko_0228@icloud.com>
2022-07-08 11:14:45 +0900

    HoT: initial revision

---
+++ Heads_or_Tails.cs
@@ -0,0 +1,26 @@
+using System;
+class HeadsorTails
+{
+    public static void Main()
+    {
+        Random random = new Random();
+        int condition = 0;
+        int HeadCount = 0, TailCount = 0;
+        Console.WriteLine("Tossing a coin...");
+        for(int i = 0; i < 3; i++)
+        {
+            condition = random.Next(0, 2);
+            if(condition == 0)
+            {
+                Console.WriteLine("Round: " + (i + 1)+ " " + "Heads");
+                HeadCount++;
+            }
+            else
+            {
+                Console.WriteLine("Round: " + (i + 1) + " " + "Tails");
+                TailCount++;
+            }
+        }
+        Console.WriteLine("Heads: " + HeadCount + " Tails: " + TailCount);
+    }
+}
